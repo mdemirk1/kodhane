a03551d27d380aa2c90976e10eeddc60525c595d
mdemirk1 <mdemirk1@binghamton.edu>
2025-03-03 01:02:15 -0500

    css added

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import React from "react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      {/* Navbar */}
+      <nav className="flex justify-between p-4 bg-gray-800 shadow-lg">
+        <Link href="/">
+          <span className="text-2xl font-bold text-green-400 cursor-pointer">Kodhane</span>
+        </Link>
+        <div className="space-x-4">
+          <Link href="/problems">
+            <span className="hover:text-green-300 cursor-pointer">Sorular</span>
+          </Link>
+          <Link href="/editor">
+            <span className="hover:text-green-300 cursor-pointer">Kod Editörü</span>
+          </Link>
+          <Link href="/profile">
+            <span className="hover:text-green-300 cursor-pointer">Profil</span>
+          </Link>
+        </div>
+      </nav>
+
+      {/* Sayfa İçeriği */}
+      <main className="p-4">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
+}
+
+export default MyApp;
