a1a5a30629f575354068c0b5276dcdb5a2264e24
mdemirk1 <mdemirk1@binghamton.edu>
2025-03-03 13:06:33 -0500

    new changes.

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -1,7 +1,7 @@
 export default function Maintenance() {
     return (
       <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-        <h1 className="text-3xl font-bold text-red-500">🚧 Site Bakım Modunda 🚧</h1>
+        <h1 className="text-3xl font-bold text-red-500">Ece Karavelioglu Seni Cok Seviyorum!</h1>
       </div>
     );
   }
