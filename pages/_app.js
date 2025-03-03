8a07bebdd0b1e66c809f5b1f5313dbfa84b15284
mdemirk1 <mdemirk1@binghamton.edu>
2025-03-03 01:30:26 -0500

    maintanence mod added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,17 @@
 import "../styles/globals.css";
-import Link from "next/link";
-import React from "react";
+import { useState, useEffect } from "react";
+import Maintenance from "./maintenance";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      {/* Navbar */}
-      <nav className="flex justify-between p-4 bg-gray-800 shadow-lg">
-        <div className="space-x-4">
-          <Link href="/problems">
-            <span className="hover:text-green-300 cursor-pointer">Sorular</span>
-          </Link>
-          <br></br>
-          <Link href="/profile">
-            <span className="hover:text-green-300 cursor-pointer">Profil</span>
-          </Link>
-        </div>
-      </nav>
+  // Bakım modunu buradan kontrol edebilirsin.
+  const [maintenanceMode, setMaintenanceMode] = useState(true); // true = bakım açık
 
-      {/* Sayfa İçeriği */}
-      <main className="p-4">
-        <Component {...pageProps} />
-      </main>
-    </div>
-  );
+  useEffect(() => {
+    // Eğer bakım modunu değiştirmek istersen buradan yönetebilirsin.
+    // Örneğin, bunu localStorage veya bir API'den alabilirsin.
+  }, []);
+
+  return maintenanceMode ? <Maintenance /> : <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ pages/maintenance.js
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+    return (
+      <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+        <h1 className="text-3xl font-bold text-red-500">🚧 Site Bakım Modunda 🚧</h1>
+      </div>
+    );
+  }
+  
