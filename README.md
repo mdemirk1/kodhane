391c9f85ad2bd5487dc95b746ceb321413b74ccc
mdemirk1 <mdemirk1@binghamton.edu>
2025-03-04 14:55:47 -0500

    new message.

---
+++ README.md
@@ -0,0 +1,1 @@
+# KodHane - Türkçe Kodlama Platformu
---
+++ pages/maintenance.js
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+    return (
+      <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+        <h1 className="text-3xl font-bold text-red-500">Ahmet'i sikeyim...</h1>
+      </div>
+    );
+  }
+  
