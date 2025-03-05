2e7dc2b885b92603aaaa596d1c6c834f0c04ef23
Mustafa Dora Demirkir <89745590+mdemirk1@users.noreply.github.com>
2025-03-04 19:03:31 -0500

    Update maintenance.js

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -1,7 +1,7 @@
 export default function Maintenance() {
     return (
       <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-        <h1 className="text-3xl font-bold text-red-500">Ahmet'i sikeyim...</h1>
+        <h1 className="text-3xl font-bold text-red-500">Site Bakimda...</h1>
       </div>
     );
   }
